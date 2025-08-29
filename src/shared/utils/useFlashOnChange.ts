d37b49736071d6091ca04bb45c698e923bc36c72
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2025-08-29 23:49:08 +0300

    chores: console log deleted

--- src/shared/utils/useFlashOnChange.ts
+++ src/shared/utils/useFlashOnChange.ts
@@ -3,7 +3,6 @@ import { useEffect, useRef, useState } from 'react';
 export function useFlashOnChange<T>(value: T, duration = 3000) {
   const [flash, setFlash] = useState(false);
   const prev = useRef<T>(value);
-  console.log(prev);
 
   useEffect(() => {
     if (prev.current !== value) {
